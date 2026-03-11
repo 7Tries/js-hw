facd2be9b8bb72f3fd705b3d8ff89654dc972377
Anxiety <20043010rr@gmail.com>
2026-03-12 00:45:35 +0200

    Add files via upload

---
+++ 123/1/123.js
@@ -0,0 +1,7 @@
+function checkAge(age) {
+  return (age > 18) ? true : confirm('А родители разрешили?');
+}
+
+// function checkAge(age) {
+//   return (age > 18) || confirm('А родители разрешили?');
+// }
